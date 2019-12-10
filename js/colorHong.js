3eb7d9f3d0abe2079fea29d6d7bad048fbd3ef81
aqx <anqingxuev@sina.com>
2019-12-10 11:57:08 +0800

    完善首页鼠标滑动改变颜色问题 ---首页除数据渲染问题外 完成

--- js/colorHong.js
+++ js/colorHong.js
@@ -3,13 +3,13 @@
     // 鼠标滑过改变字体颜色
     var color = "";
     // console.log($(".colorHong"))
-    $(".colorHong").mouseenter(function (){
+    $(".red").mouseenter(function (){
         // console.log(0)
         color = $(this).css("color");   // 先把之前的颜色保存下来，鼠标离开时还原
-        $(this).css("color","red");
+        $(this).css("color","#e13358");
     });
     // 鼠标离开时，恢复为原来的颜色
-    $(".colorHong").mouseleave(function () {
+    $(".red").mouseleave(function () {
         $(this).css("color",color);
     })
 
